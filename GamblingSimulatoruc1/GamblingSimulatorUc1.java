a4522d0ac7f5bc3161432502aa0c5093a62262b7
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-09-06 10:54:51 +0530

    gambler would start with a stake

---
+++ GamblingSimulatoruc1/GamblingSimulatorUc1.java
@@ -0,0 +1,14 @@
+package GamblingSimulatoruc1;
+import java.util.*;
+
+public class GamblingSimulatorUc1 {
+	
+	public static void main(String[] args) {
+		 System.out.println("Welcom to the GamblerSimulator");
+
+        int amount = 100;
+		 int bet = 1;
+		 System.out.println("Amount of stake is $"+amount);
+		 System.out.println("Amount to bet after every game $"+bet);
+	}
+}
